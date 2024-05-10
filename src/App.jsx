a0bb0785927afa6354e34294ecfc4fa9343c2f97
Meheraj-Hossain <mehossain13@gmail.com>
2024-05-10 20:47:47 +0600

    counter by primitive value and object understood

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Counter from "./Components/Counter.jsx";
+// import CounterByPrimitive from "./Components/CounterByPrimitive.jsx";
+import CounterByObject from "./Components/CounterByObject.jsx";
 export default function App () {
 
 
     return (
         <div className="app">
-            <Counter />
+            <CounterByObject />
         </div>
     )
 }
